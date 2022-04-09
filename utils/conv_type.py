1597ccddb202a9bf89bc60a6f96362785069fafd
matthewgorbett <Idontknow8112@@@>
2022-04-09 12:50:21 -0600

    rerandomization

--- utils/conv_type.py
+++ utils/conv_type.py
@@ -63,7 +63,7 @@ class SubnetConv(nn.Conv2d):
                 j = int((self.args.rerand_rate) * scores_temp.size(0))
                 cutoff=sorted[j].item()
                 inds = (self.scores < cutoff).nonzero(as_tuple=False)
-                print('rerandomizing {} out of {} weights'.format(inds.size()[0],self.weight.numel()))
+                print('rerandomized {} out of {} weights'.format(inds.size()[0],self.weight.numel()))
                 self.weight[inds[:,0], inds[:,1]]=weight_twin[inds[:,0], inds[:,1]]
 
     def get_sparsity(self):
@@ -141,5 +141,118 @@ class SubnetBinaryConv(nn.Conv2d):
 
 
 
+class GetSubnetOrig(autograd.Function):
+    @staticmethod
+    def forward(ctx, scores, k):
+        # Get the subnetwork by sorting the scores and using the top k%
+        out = scores.clone()
+        _, idx = scores.flatten().sort()
+        j = int((1 - k) * scores.numel())
+
+        # flat_out and out access the same memory.
+        flat_out = out.flatten()
+        flat_out[idx[:j]] = 0
+        flat_out[idx[j:]] = 1
+
+        return out
+
+    @staticmethod
+    def backward(ctx, g):
+        # send the gradient g straight-through on the backward pass.
+        return g, None
 
 
+# Not learning weights, finding subnet
+class SubnetConvOrig(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.scores = nn.Parameter(torch.Tensor(self.weight.size()))
+        nn.init.kaiming_uniform_(self.scores, a=math.sqrt(5))
+
+        self.prune_rate = 0.5
+
+    @property
+    def clamped_scores(self):
+        return self.scores.abs()
+
+    def get_sparsity(self):
+        subnet = GetSubnetOrig.apply(self.scores)
+        temp = subnet.detach().cpu()
+        return temp.mean()
+
+    def forward(self, x):
+        subnet = GetSubnetOrig.apply(self.clamped_scores, self.prune_rate)
+        w = self.weight * subnet
+        x = F.conv2d(
+            x, w, self.bias, self.stride, self.padding, self.dilation, self.groups
+        )
+        return x
+
+
+
+
+
+class GetQuantnet_binaryOrig(autograd.Function):
+    @staticmethod
+    def forward(ctx, scores, weights, k):
+        # Get the subnetwork by sorting the scores and using the top k%
+        out = scores.clone()
+        _, idx = scores.flatten().sort()
+        j = int((1 - k) * scores.numel())
+        # flat_out and out access the same memory. switched 0 and 1
+        flat_out = out.flatten()
+        flat_out[idx[:j]] = 0
+        flat_out[idx[j:]] = 1
+
+        # Perform binary quantization of weights
+        abs_wgt = torch.abs(weights.clone()) # Absolute value of original weights
+        q_weight = abs_wgt * out # Remove pruned weights
+        num_unpruned = int(k * scores.numel()) # Number of unpruned weights
+        alpha = torch.sum(q_weight) / num_unpruned # Compute alpha = || q_weight ||_1 / (number of unpruned weights)
+
+        # Save absolute value of weights for backward
+        ctx.save_for_backward(abs_wgt)
+
+        # Return pruning mask with gain term alpha for binary weights
+        return alpha * out
+
+    @staticmethod
+    def backward(ctx, g):
+        # Get absolute value of weights from saved ctx
+        abs_wgt, = ctx.saved_tensors
+        # send the gradient g times abs_wgt on the backward pass
+        return g * abs_wgt, None, None
+
+class SubnetBinaryConvOrig(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.scores = nn.Parameter(torch.Tensor(self.weight.size()))
+        nn.init.kaiming_uniform_(self.scores, a=math.sqrt(5))
+      #  print ("subnet conv init: ", torch.isnan(self.scores).any())
+
+        self.prune_rate = 0.5
+
+    @property
+    def clamped_scores(self):
+        # For unquantized activations
+        return self.scores.abs()
+    def get_sparsity(self):
+        subnet = GetQuantnet_binaryOrig.apply(self.scores)
+        temp = subnet.detach().cpu()
+        return temp.mean()
+    def forward(self, x):
+        # For debugging gradients, prints out maximum value in gradients
+        if parser_args.debug:
+            if quantnet.grad: print ("subnetconv fwd quantnet grad ", torch.max(quantnet.grad))
+        # Get binary mask and gain term for subnetwork
+        quantnet = GetQuantnet_binaryOrig.apply(self.clamped_scores, self.weight, self.prune_rate)
+        # Binarize weights by taking sign, multiply by pruning mask and gain term (alpha)
+        w = torch.sign(self.weight) * quantnet
+        # Pass binary subnetwork weights to convolution layer
+        x = F.conv2d(
+            x, w, self.bias, self.stride, self.padding, self.dilation, self.groups
+        )
+        # Return output from convolution layer
+        return x
