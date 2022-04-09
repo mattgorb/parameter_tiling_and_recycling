d3400b99ee0f293e20c400032675a3df104aac33
matthewgorbett <Idontknow8112@@@>
2022-04-09 09:04:50 -0600

    changes

---
+++ trainers/default.py
@@ -0,0 +1,186 @@
+import time
+import torch
+import tqdm
+
+from utils.eval_utils import accuracy
+from utils.logging import AverageMeter, ProgressMeter
+
+from utils.conv_type import SubnetConv
+
+__all__ = ["train", "validate", "modifier","validate_pretrained"]
+
+
+def train(train_loader, model, criterion, optimizer, epoch, args, writer):
+    batch_time = AverageMeter("Time", ":6.3f")
+    data_time = AverageMeter("Data", ":6.3f")
+    losses = AverageMeter("Loss", ":.3f")
+    top1 = AverageMeter("Acc@1", ":6.2f")
+    top5 = AverageMeter("Acc@5", ":6.2f")
+
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix=f"Epoch: [{epoch}]",
+    )
+
+    # switch to train mode
+    model.train()
+
+    batch_size = train_loader.batch_size
+    num_batches = len(train_loader)
+    end = time.time()
+    for i, (images, target) in tqdm.tqdm(
+        enumerate(train_loader), ascii=True, total=len(train_loader)
+    ):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        if args.gpu is not None:
+            images = images.to("cuda:{}".format(args.gpu))#cuda(args.gpu, non_blocking=True)
+
+        target = target.to("cuda:{}".format(args.gpu))#.cuda(args.gpu, non_blocking=True)
+
+        # compute output
+        output = model(images)
+
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), images.size(0))
+        top1.update(acc1.item(), images.size(0))
+        top5.update(acc5.item(), images.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            t = (num_batches * epoch + i) * batch_size
+            progress.display(i)
+            progress.write_to_tensorboard(writer, prefix="train", global_step=t)
+
+    return top1.avg, top5.avg
+
+
+def validate(val_loader, model, criterion, args, writer, epoch):
+    # switch to evaluate mode
+    model.eval()
+
+    losses = AverageMeter("Loss", ":.3f", write_val=False)
+    top1 = AverageMeter("Acc@1", ":6.2f", write_val=False)
+    top5 = AverageMeter("Acc@5", ":6.2f", write_val=False)
+
+    avg_meters=[ losses, top1, top5,]
+
+    track_subnet=False
+    for n, m in model.named_modules():
+        if isinstance(m, SubnetConv):
+            track_subnet=True
+            break
+    if track_subnet:
+        module_meters={}
+        for n, m in model.named_modules():
+            if isinstance(m, SubnetConv):
+                module_meters[n]=AverageMeter(n, ":6.2f", write_val=False)
+
+        avg_meters.extend(module_meters.values())
+
+        percent_pruned= AverageMeter("PercentPruned", ":6.2f")
+        avg_meters.append(percent_pruned)
+
+    progress = ProgressMeter(
+        len(val_loader), avg_meters, prefix="Test: "
+    )
+
+
+
+    if track_subnet:
+        total_not_pruned=0
+        total_parameters=0
+        for n, m in model.named_modules():
+            if isinstance(m, SubnetConv):
+                module_meters[n].update(m.get_sparsity())
+                total_parameters+=m.scores.size().numel()
+                total_not_pruned+=(m.scores.size().numel()*m.get_sparsity())
+
+        total_percent=(1-(total_not_pruned/total_parameters).item())
+        percent_pruned.update(total_percent)
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in tqdm.tqdm(
+            enumerate(val_loader), ascii=True, total=len(val_loader)
+        ):
+            if args.gpu is not None:
+                images = images.to("cuda:{}".format(args.gpu))#.cuda(args.gpu, non_blocking=True)
+
+            target = target.to("cuda:{}".format(args.gpu))#.cuda(args.gpu, non_blocking=True)
+
+            # compute output
+            output = model(images)
+
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), images.size(0))
+            top1.update(acc1.item(), images.size(0))
+            top5.update(acc5.item(), images.size(0))
+
+            # measure elapsed time
+            #batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                progress.display(i)
+
+        progress.display(len(val_loader))
+
+        if writer is not None:
+            progress.write_to_tensorboard(writer, prefix="test", global_step=epoch)
+    print("Acc@1: {}, Acc@5: {}".format(top1.avg, top5.avg))
+    return top1.avg, top5.avg
+
+
+
+def validate_pretrained(val_loader, model, criterion, args):
+    # switch to evaluate mode
+    model.eval()
+
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in tqdm.tqdm(
+            enumerate(val_loader), ascii=True, total=len(val_loader)
+        ):
+            if args.gpu is not None:
+                images = images.cuda(args.gpu, non_blocking=True)
+
+            target = target.cuda(args.gpu, non_blocking=True)
+
+            # compute output
+            output = model(images)
+
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
+            print(acc1)
+            print(acc5)
+
+            sys.exit()
+
+
+    return top1.avg, top5.avg
+
+
+
+def modifier(args, epoch, model):
+    return
---
+++ utils/conv_type.py
@@ -0,0 +1,153 @@
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+
+from args import args as parser_args
+from utils.initializations import _init_weight,_init_score
+
+DenseConv = nn.Conv2d
+
+
+class GetSubnet(autograd.Function):
+    @staticmethod
+    def forward(ctx, scores, th=0):
+        out = scores.clone()
+        out=(out > th).float()
+
+        return out
+    @staticmethod
+    def backward(ctx, g):
+        # send the gradient g straight-through on the backward pass.
+        return g, None
+
+
+# Not learning weights, finding subnet
+class SubnetConv(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.scores = nn.Parameter(torch.Tensor(self.weight.size()))
+        nn.init.kaiming_uniform_(self.scores, a=math.sqrt(5))
+        self.scores=nn.Parameter(self.scores)
+
+    def init(self,args):
+        self.args=args
+        print(self.weight)
+        _init_weight(self.args, self.weight)
+        print(self.weight)
+        _init_score(self.args, self.scores)
+        if args.threshold is None:
+            self.th=0
+        else:
+            self.th=self.args.threshold
+
+    def rerandomize(self):
+        with torch.no_grad():
+            self.args.weight_seed+=1
+            weight_twin=torch.zeros_like(self.weight)
+            scores_lt0=(self.scores<=0).nonzero(as_tuple=False)
+
+            if self.args.rerand_type=='iterand':
+                j = int((self.args.rerand_rate) * scores_lt0.size(0))
+                indices_to_replace=torch.randperm(len(scores_lt0))[:j]
+                inds=scores_lt0[indices_to_replace]
+                weight_temp=nn.Parameter(self.weight)
+                weight_temp.requires_grad_(False)
+                weight_temp[inds[:,0], inds[:,1]]=weight_twin[inds[:,0], inds[:,1]]
+                self.weight=nn.Parameter(weight_temp)
+            elif self.args.rerand_type=='iterand_threshold':
+                scores_temp=self.scores[scores_lt0[:,0], scores_lt0[:,1]]
+                sorted, indices = torch.sort(scores_temp.flatten())
+                j = int((self.args.rerand_rate) * scores_temp.size(0))
+                cutoff=sorted[j].item()
+                #print(cutoff)
+                #inds=((self.scores<=0) & (self.scores<cutoff)).nonzero(as_tuple=False)
+                inds = (self.scores < cutoff).nonzero(as_tuple=False)
+                #print(self.scores[inds[:,0], inds[:,1]])
+                #print(self.scores[scores_lt0[:,0], scores_lt0[:,1]])
+                #sys.exit()
+                #inds=scores_lt0[indices_to_replace]
+                weight_temp=nn.Parameter(self.weight)
+                weight_temp.requires_grad_(False)
+                weight_temp[inds[:,0], inds[:,1]]=weight_twin[inds[:,0], inds[:,1]]
+                self.weight=nn.Parameter(weight_temp)
+
+    def get_sparsity(self):
+        subnet = GetSubnet.apply(self.scores,self.th)
+        temp = subnet.detach().cpu()
+        return temp.mean()
+
+    def forward(self, x):
+        subnet = GetSubnet.apply(self.scores,self.th)
+        w = self.weight * subnet
+        x = F.conv2d(
+            x, w, self.bias, self.stride, self.padding, self.dilation, self.groups
+        )
+        return x
+
+
+
+
+class GetQuantnet_binary(autograd.Function):
+    @staticmethod
+    def forward(ctx, scores, weights,th=0 ):
+        # Get the subnetwork by sorting the scores and using the top k%
+        out = scores.clone()
+        out=(out > th).float()
+
+        # Perform binary quantization of weights
+        abs_wgt = torch.abs(weights.clone()) # Absolute value of original weights
+        q_weight = abs_wgt * out # Remove pruned weights
+        num_unpruned = int(torch.sum(out)) # Number of unpruned weights
+        alpha = torch.sum(q_weight) / num_unpruned # Compute alpha = || q_weight ||_1 / (number of unpruned weights)
+
+        # Save absolute value of weights for backward
+        ctx.save_for_backward(abs_wgt)
+
+        # Return pruning mask with gain term alpha for binary weights
+        return alpha * out
+
+    @staticmethod
+    def backward(ctx, g):
+        # Get absolute value of weights from saved ctx
+        abs_wgt, = ctx.saved_tensors
+        # send the gradient g times abs_wgt on the backward pass
+        return g * abs_wgt, None, None
+
+
+class SubnetBinaryConv(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.scores = nn.Parameter(torch.Tensor(self.weight.size()))
+        nn.init.kaiming_uniform_(self.scores, a=math.sqrt(5))
+
+    def get_sparsity(self):
+        subnet = GetSubnet.apply(self.scores)
+        temp = subnet.detach().cpu()
+        return temp.mean()
+
+    def forward(self, x):
+        # For debugging gradients, prints out maximum value in gradients
+        # Get binary mask and gain term for subnetwork
+        quantnet = GetQuantnet_binary.apply(self.scores, self.weight)
+        # Binarize weights by taking sign, multiply by pruning mask and gain term (alpha)
+        w = torch.sign(self.weight) * quantnet
+        # Pass binary subnetwork weights to convolution layer
+        x = F.conv2d(
+            x, w, self.bias, self.stride, self.padding, self.dilation, self.groups
+        )
+        # Return output from convolution layer
+        return x
+
+
+
+
+
+
+
+
+
+
