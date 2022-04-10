1ef5b4f4e8f020957977e719f98b53b224987c4d
matthewgorbett <Idontknow8112@@@>
2022-04-09 19:50:28 -0600

    rerandomization

--- utils/conv_type.py
+++ utils/conv_type.py
@@ -14,7 +14,7 @@ DenseConv = nn.Conv2d
 class GetSubnet(autograd.Function):
     @staticmethod
     def forward(ctx, scores, k):
-        k=(.48-.55)*torch.randint(1)+.49
+        k=torch.FloatTensor(1).uniform_(.49, .53)
         print(k)
         print(k.item())
         # Get the subnetwork by sorting the scores and using the top k%
