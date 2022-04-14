363bf5e5f56b8c339a4afb636d2ddc9d11d9d386
matthewgorbett <Idontknow8112@@@>
2022-04-14 17:24:36 -0600

    iterand c2

--- utils/conv_type.py
+++ utils/conv_type.py
@@ -105,8 +105,9 @@ class SubnetConvEdgePopup(nn.Conv2d):
             if self.args.rerand_type == 'recycle':
                 sorted, indices = torch.sort(self.scores.abs().flatten())
                 j = int((self.args.rerand_rate) * self.scores.numel())
+                k = int((1-self.args.rerand_rate) * self.scores.numel())
                 low_scores = (self.scores.abs() <  sorted[j]).nonzero(as_tuple=True)
-                high_scores = (self.scores.abs() > sorted[-j]).nonzero(as_tuple=True)
+                high_scores = (self.scores.abs() > sorted[k]).nonzero(as_tuple=True)
                 self.weight[low_scores]=self.weight[high_scores]
                 print('recycling {} out of {} weights'.format(j,self.weight.numel()))
             elif self.args.rerand_type == 'iterand':
