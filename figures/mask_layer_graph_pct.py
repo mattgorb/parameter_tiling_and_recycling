edc104c7ee023f7016d9cabed4af7c23d1d66a4d
matthewgorbett <Idontknow8112@@@>
2022-04-23 19:20:31 -0600

    sizee

--- figures/mask_layer_graph_pct.py
+++ figures/mask_layer_graph_pct.py
@@ -2,11 +2,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from seaborn import lineplot,scatterplot
+
+jaccard=True
+prune_rate=None
+if jaccard:
+    #col='jaccard_total'
+    savefile=f'tables/masks_graph_jaccard_{prune_rate}.pdf'
+    cols=['model_combo','convs.0_jaccard','convs.2_jaccard','convs.5_jaccard','convs.7_jaccard'
+          ,'convs.10_jaccard','convs.12_jaccard','linear.0_jaccard','linear.2_jaccard','linear.4_jaccard']
+else:
+    #col='percent_same_total'
+    savefile=f'tables/masks_graph_pct_same_{prune_rate}.pdf'
+    cols=['model_combo','convs.0_pctsame','convs.2_pctsame','convs.5_pctsame','convs.7_pctsame'
+          ,'convs.10_pctsame','convs.12_pctsame','linear.0_pctsame','linear.2_pctsame','linear.4_pctsame']
+
 fig = plt.figure(figsize=(8,8))
 sns.set_style('darkgrid')
-df1=pd.read_csv('tables/model_combos_conv6-sc-epu-var.csv')
+df1=pd.read_csv(f'tables/model_combos_conv6-sc-epu-var_{prune_rate}.csv')
 print(df1.columns)
-df1=df1[df1.columns[1:11]]
+df1=df1[cols]
 print(df1.shape)
 dfm = df1.melt('model_combo', var_name='cols', value_name='vals')
 print(dfm.shape)
@@ -19,9 +33,9 @@ print(dfm2)
 
 
 
-df1=pd.read_csv('tables/model_combos_conv6-kn-biprop-var.csv')
+df1=pd.read_csv(f'tables/model_combos_conv6-kn-biprop-var_{prune_rate}.csv')
 print(df1.columns)
-df1=df1[df1.columns[1:11]]
+df1=df1[cols]
 print(df1.shape)
 dfm = df1.melt('model_combo', var_name='cols', value_name='vals')
 print(dfm.shape)
@@ -38,12 +52,19 @@ ax =lineplot(data=dfm3, x='cols', y="mean", marker="v",markersize=7,label='Bipro
 ax.fill_between(dfm3['cols'], dfm3['min'], dfm3['max'], alpha=0.2)
 ax.set(xlim=(-.05, 8.05))
 #ax.set(xlabel='Model Layer', size=12)
-ax.set_ylabel('Percent Matching', fontsize=16)
+
 ax.set_xlabel('Model Layer', fontsize=16)
 ax.set(xticklabels=['Conv. 1','Conv. 2','Conv. 3','Conv. 4','Conv. 5','Conv. 6','Lin. 1','Lin. 2', 'Lin.3'])
-ax.set(yticklabels=['','50','55','60','65','70','75'])
+#ax.set(yticklabels=['','50','55','60','65','70','75'])
 plt.xticks(rotation=30, size = 12)
 print(ax.get_xlim())
+if jaccard:
+    ax.set_ylabel('Jaccard Index', fontsize=16)
+else:
+    yticks=ax.get_yticks()
+    yticks=[f"{round(i*100,2)}%" for i in yticks]
+    ax.set(yticklabels=yticks)
+    ax.set_ylabel('Percent Matching', fontsize=16)
 plt.legend(fontsize='x-large',)
 plt.tight_layout()
-plt.savefig('tables/masks_graph_pct_same.pdf')
+plt.savefig(savefile)
--- figures/mask_var.py
+++ figures/mask_var.py
@@ -5,15 +5,24 @@ from scipy.spatial.distance import pdist,squareform
 import numpy as np
 from matplotlib.colors import ListedColormap
 
-df1=pd.read_csv('tables/model_combos_conv6-sc-epu-var.csv')
-df1_pctsame=df1['percent shared total'].values
+jaccard=True
+prune_rate=None
+if jaccard:
+    col='jaccard_total'
+    savefile=f'tables/heatmap_jaccard_{prune_rate}.pdf'
+else:
+    col='percent_same_total'
+    savefile=f'tables/heatmap_pctsame_{prune_rate}.pdf'
+
+df1=pd.read_csv(f'tables/model_combos_conv6-sc-epu-var_{prune_rate}.csv')
+df1_pctsame=df1[col].values
 min_val1=min(df1_pctsame)
 max_val1=max(df1_pctsame)
 avg_val1=np.mean(np.array(df1_pctsame))
 
 
-df1=pd.read_csv('tables/model_combos_conv6-kn-biprop-var.csv')
-df1_pctsame2=df1['percent shared total'].values
+df1=pd.read_csv(f'tables/model_combos_conv6-kn-biprop-var_{prune_rate}.csv')
+df1_pctsame2=df1[col].values
 #print(df1_pctsame.shape)
 df1_pctsame2=np.array(df1_pctsame2)
 df1_pctsame2=np.array(df1_pctsame2)
@@ -38,8 +47,12 @@ cmap2=cmap[int(len(cmap)/2):]
 cmap2 = ListedColormap(cmap2)
 x=np.tril(df1_pctsame2)
 
+
+cmap_args={'use_gridspec':True,"shrink": .72, "location":"bottom",'pad':0.07,'anchor':(0.15, 1.0)}
+
+
 sns.heatmap(np.tril(df1_pctsame),   cmap=cmap1,mask=np.triu(df1_pctsame),vmin=min_val1,axes=ax, vmax=max_val1, center=avg_val1,
-            square=False, cbar_kws={'use_gridspec':True,"shrink": .72, "location":"bottom",'pad':0.07,'anchor':(0.15, 1.0)})
+            square=False, cbar_kws=cmap_args)
 sns.heatmap(np.triu(df1_pctsame2),  cmap=cmap2,mask=np.tril(df1_pctsame2), vmin=min_val2,vmax=max_val2, axes=ax,center=avg_val2 ,
             square=False, cbar_kws={'use_gridspec':True,"shrink": .85 })
 sns.heatmap(result ,mask=result, cbar=False,cmap=ListedColormap(['lavender']),vmin=0,vmax=0,square=False,linewidths=.004, )
@@ -47,13 +60,21 @@ sns.heatmap(result ,mask=result, cbar=False,cmap=ListedColormap(['lavender']),vm
 
 cbar = ax.collections[0].colorbar
 cbar.ax.tick_params(labelsize=20)
-cbar.set_ticks([0.506,  .5090])
-cbar.set_ticklabels(['50.6%' ,'50.9%'])
 
+cbar.set_ticks([min_val1,  max_val1])
+if not jaccard:
+    #cbar.set_label("% Matching Masks",size=16)
+    cbar.set_ticklabels([f'{round(min_val1*100,3)}%' ,f'{round(max_val1*100,3)}%'])
+else:
+    cbar.set_ticklabels([f'{round(min_val1 , 4)}%', f'{round(max_val1 , 4)}%'])
 cbar = ax.collections[1].colorbar
 cbar.ax.tick_params(labelsize=18)
-cbar.set_ticks([0.5775,  .5795])
-cbar.set_ticklabels(['57.75%','57.95%'])
+cbar.set_ticks([min_val2,  max_val2])
+if not jaccard:
+    #cbar.set_label("% Matching Masks, test", size=16)
+    cbar.set_ticklabels([f'{round(min_val2*100,3)}%',f'{round(max_val2*100,3)}%'])
+else:
+    cbar.set_ticklabels([f'{round(min_val2,4)}%',f'{round(max_val2,4)}%'])
 ax.set_title('Biprop', fontsize=26)
 ax.set_xlabel('Score Parameter Seed Number', fontsize=18)
 ax.set_ylabel('Edge-Popup', fontsize=26)
@@ -61,4 +82,4 @@ ax.set_ylabel('Edge-Popup', fontsize=26)
 #f.set_axis_labels(None, 'Values')
 
 plt.tight_layout()#rect=[0,.1,1,1]
-plt.savefig('tables/heatmap.png')
+plt.savefig(savefile)
