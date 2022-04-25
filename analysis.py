0219c84c104b4f580d94d2991610b22b069184c5
matthewgorbett <Idontknow8112@@@>
2022-04-24 20:22:21 -0600

    variability analysis bug

--- analysis.py
+++ analysis.py
@@ -62,9 +62,9 @@ def main_worker(args):
     #--config configs/cifar10/conv8/conv8-sc-epu.yaml
     #weightfile='/s/luffy/b/nobackup/mgorb/runs/conv8-sc-epu/baseline/prune_rate=0.5/0/checkpoints/model_best.pth'
     # --config configs/cifar10/conv8/conv8-sc-epu-iterand.yaml
-    weightfile='/s/luffy/b/nobackup/mgorb/runs/conv8-sc-epu-iterand/baseline/prune_rate=0.5/0/checkpoints/model_best.pth'
+    #weightfile='/s/luffy/b/nobackup/mgorb/runs/conv8-sc-epu-iterand/baseline/prune_rate=0.5/0/checkpoints/model_best.pth'
     #--config configs/cifar10/conv8/conv8-sc-epu-recycle.yaml
-    weightfile='/s/luffy/b/nobackup/mgorb/runs/conv8-sc-epu-recycle/baseline/prune_rate=0.5/0/checkpoints/model_best.pth'
+    #weightfile='/s/luffy/b/nobackup/mgorb/runs/conv8-sc-epu-recycle/baseline/prune_rate=0.5/0/checkpoints/model_best.pth'
 
     #--config configs/cifar10/conv8/conv8-dense-kn.yaml
     #dense=True
@@ -72,11 +72,9 @@ def main_worker(args):
     model = get_model(args)
     model,device = set_gpu(args, model)
 
-
-
     criterion = nn.CrossEntropyLoss().to(device)
 
-    pretrained(weightfile, model)
+    pretrained(args.pretrained, model)
     acc1, acc5 = validate(data.val_loader, model, criterion, args, writer=None, epoch=args.start_epoch )
 
     model.eval()
