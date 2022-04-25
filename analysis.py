7f1e7071782ceefcc35246166f6e198f88768d34
matthewgorbett <Idontknow8112@@@>
2022-04-24 21:57:57 -0600

    variability analysis bug

--- analysis.py
+++ analysis.py
@@ -82,12 +82,12 @@ def main_worker(args):
     for name,mod in model.named_modules():
 
         if isinstance(mod, SubnetConvEdgePopup) or isinstance(mod,SubnetConvBiprop):
-            '''weight_flat = mod.weight.flatten()
+            weight_flat = mod.weight.flatten()
             half = int(weight_flat.numel() * 0.5)
             vals, idx = weight_flat.abs().sort(descending=False)
             top = vals[:half]
-            print(torch.norm(top).item())'''
-            #scontinue
+            print(torch.norm(mod.weight.flatten()).item())
+            continue
             if isinstance(mod, SubnetConvBiprop):
                 mask1 = GetQuantnet_binary.apply(mod.clamped_scores, mod.weight, mod.prune_rate)
                 #y = torch.ones_like(mask1)
