5704e177926a6c9169347d9e1f618c7aab04ff56
matthewgorbett <Idontknow8112@@@>
2022-04-24 21:55:06 -0600

    variability analysis bug

--- analysis.py
+++ analysis.py
@@ -97,9 +97,9 @@ def main_worker(args):
                 mask1 = GetSubnetEdgePopup.apply(mod.clamped_scores, mod.prune_rate)
             #print(mod.weight.size())
 
-            mask1=torch.nonzero(mask1.flatten())
-            print(mask1.size())
-            weights_with_mask=mod.weight.flatten()[mask1]
+            mask1_ind=torch.nonzero(mask1.flatten())
+            print(mask1)
+            weights_with_mask=mod.weight.flatten()[mask1_ind]
             print(weights_with_mask.size())
             sys.exit()
             print(name)
