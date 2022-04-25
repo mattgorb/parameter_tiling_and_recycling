098bfc18eee8ba6a4d461d18379417717780e5f1
matthewgorbett <Idontknow8112@@@>
2022-04-24 22:06:44 -0600

    variability analysis bug

--- utils/conv_type.py
+++ utils/conv_type.py
@@ -116,7 +116,8 @@ class SubnetConvEdgePopup(nn.Conv2d):
                 self.args.weight_seed += 1
                 weight_twin = torch.zeros_like(self.weight)
                 weight_twin = _init_weight(self.args, weight_twin)
-
+                print(weight_twin)
+                sys.exit()
                 ones = torch.ones(self.weight.size()).to(self.weight.device)
                 b = torch.bernoulli(ones * self.args.rerand_rate)
                 mask=GetSubnetEdgePopup.apply(self.clamped_scores,  self.prune_rate)
