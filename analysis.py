786f3a9e439f1563cf92081f127c08a5f1015dd1
matthewgorbett <Idontknow8112@@@>
2022-04-24 19:42:24 -0600

    variability analysis bug

--- analysis.py
+++ analysis.py
@@ -99,8 +99,8 @@ def main_worker(args):
 
             weights_with_mask=mod.weight.flatten()[mask1]
             print(name)
-            print(torch.nonzero(weights_with_mask>0).size())
-            print(torch.nonzero(weights_with_mask<0).size())
+            print(torch.nonzero(weights_with_mask>0).size()[0]/weights_with_mask.numel())
+            #print(torch.nonzero(weights_with_mask<0).size())
 
         if isinstance(mod, nn.Conv2d):
             print()
