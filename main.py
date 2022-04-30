54edf6019663c9a704e938d3dbbd260801f0e8cd
matthewgorbett <Idontknow8112@@@>
2022-04-30 13:08:53 -0600

    ddp

--- main.py
+++ main.py
@@ -275,8 +275,8 @@ def set_gpu(args, model):
         print('set distributed data parallel')
         os.environ['MASTER_ADDR'] = 'localhost'
         os.environ['MASTER_PORT'] = '12355'
-        torch.distributed.init_process_group(backend="nccl")#, #init_method="env://",
-                                             #world_size=1,
+        torch.distributed.init_process_group(backend="nccl", #init_method="env://",
+                                             world_size=2,)
                                              #rank=0)
         torch.cuda.set_device(0)
         args.workers = 4
