208fdd381af4abb4598746cc858257b1d5ec6cb5
matthewgorbett <Idontknow8112@@@>
2022-04-30 13:32:14 -0600

    ddp

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ import data
 import models
 from utils.initializations import set_seed
 
-def main(ngpus_per_node):
+def main(args,ngpus_per_node):
 
     print('args: {}'.format(args))
 
@@ -503,6 +503,6 @@ if __name__ == "__main__":
     #master_addr = '127.0.0.1'
     #master_port = find_free_port()
     ngpus_per_node = torch.cuda.device_count()
-    mp.spawn(main(), nprocs=ngpus_per_node, arg=(ngpus_per_node,))
+    mp.spawn(main(), nprocs=ngpus_per_node, arg=(args,ngpus_per_node,))
 
 
