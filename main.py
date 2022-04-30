e9dfa73467a6e3ec65053b0c0e03272af74da1de
matthewgorbett <Idontknow8112@@@>
2022-04-30 13:01:26 -0600

    ddp

--- main.py
+++ main.py
@@ -281,7 +281,7 @@ def set_gpu(args, model):
         torch.cuda.set_device(0)
         args.workers = 4
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu],output_device=args.gpu)
-        args.batch_size = int(args.batch_size / 8)
+        #args.batch_size = int(args.batch_size / 8)
         args.workers = 4
         #sys.exit()
         #model = torch.nn.DataParallel(model)#, device_ids=[1, 2, 3, 4, 5, 6, 7])
