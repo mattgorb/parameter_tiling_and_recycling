2cff85964ce265abf7df92530eff25207893a7ef
matthewgorbett <Idontknow8112@@@>
2022-05-01 20:20:36 -0600

    test

--- trainers/default_parallel.py
+++ trainers/default_parallel.py
@@ -61,7 +61,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args, writer):
         batch_time.update(time.time() - end)
         end = time.time()
 
-        if args.rank % ngpus_per_node == 0:
+        if args.rank % args.ngpus_per_node == 0:
             if i % args.print_freq == 0:
                 t = (num_batches * epoch + i) * batch_size
                 progress.display(i)
@@ -146,7 +146,7 @@ def validate(val_loader, model, criterion, args, writer, epoch):
             #batch_time.update(time.time() - end)
             end = time.time()
 
-            if args.rank % ngpus_per_node == 0:
+            if args.rank % args.ngpus_per_node == 0:
                 if i % args.print_freq == 0:
                     progress.display(i)
 
