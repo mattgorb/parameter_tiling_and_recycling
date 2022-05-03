1c8bcd04ac52ca8ba1701c7b6007f2757b84a282
matthewgorbett <Idontknow8112@@@>
2022-05-03 12:30:48 -0600

    test

--- utils/conv_type.py
+++ utils/conv_type.py
@@ -244,3 +244,65 @@ class SubnetConvBiprop(nn.Conv2d):
         )
         # Return output from convolution layer
         return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class GetSubnetEdgePopup(autograd.Function):
+    @staticmethod
+    def forward(ctx, scores, k):
+        # Get the subnetwork by sorting the scores and using the top k%
+        out = scores.clone()
+        _, idx = scores.flatten().sort()
+        j = int((1 - k) * scores.numel())
+
+        # flat_out and out access the same memory.
+        flat_out = out.flatten()
+        flat_out[idx[:j]] = 0
+        flat_out[idx[j:]] = 1
+
+        return out
+
+    @staticmethod
+    def backward(ctx, g):
+        # send the gradient g straight-through on the backward pass.
+        return g, None
+
+
+# Not learning weights, finding subnet
+class SubnetConvLTH(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.mask = nn.Parameter(torch.Tensor(self.weight.size()))
+        #nn.init.kaiming_uniform_(self.scores, a=math.sqrt(5))
+
+    def init(self,args):
+        self.args=args
+        self.weight=_init_weight(self.args, self.weight)
+
+        self.prune_rate=args.prune_rate
+
+    @property
+    def clamped_scores(self):
+        return self.scores.abs()
+
+    def forward(self, x):
+        subnet = GetSubnetEdgePopup.apply(self.clamped_scores, self.prune_rate)
+        w = self.weight * subnet
+        x = F.conv2d(
+            x, w, self.bias, self.stride, self.padding, self.dilation, self.groups
+        )
+        return x
+
