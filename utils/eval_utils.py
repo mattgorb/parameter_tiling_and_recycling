8b1c2b9ba195499722a04dc8d0ab9dbcf698e340
matthewgorbett <Idontknow8112@@@>
2022-05-03 19:01:55 -0600

    test

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -11,9 +11,12 @@ def accuracy(output, target, topk=(1,)):
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))
 
+        print(output.size())
+        print(target.size())
+        print(correct.size())
         res = []
         for k in topk:
-            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 
