6335997da39634f2b4b3f589580fd4720131b281
matthewgorbett <Idontknow8112@@@>
2022-05-14 20:43:51 -0600

    galore

--- figures/conv_width_all.py
+++ figures/conv_width_all.py
@@ -7,16 +7,17 @@ plt.clf()
 sns.set_style('whitegrid')
 fig, axs = plt.subplots(1, col, sharex=True, figsize=(5*col,6.25))
 sns.lineplot(x=[0.1,0.25,0.5,1], y=[47.2,64.55,74.69,79.56] ,ax=axs[0], linestyle='-.',label='Biprop', legend=False)
-sns.lineplot(x=[0.1,0.25,0.5,1], y=[52.55,68.8,76.66,81.36] ,ax=axs[0], label='Biprop+Recycle',linewidth =2, legend=False)
+axs[0].errorbar([0.1,0.25,0.5,1], [52.55,68.8,76.66,81.36] ,[.8,.67,.15,.2], label='Biprop+Recycle',linewidth =2, )
+sns.lineplot(x=[0.1,0.25,0.5,1], y=[48.1,64.52,73.65,79.69] ,ax=axs[0], linestyle='-.',label='Edge-Popup', legend=False)
+axs[0].errorbar([0.1,0.25,0.5,1], [53.4,70.33,77,81.68] ,[.76,.56,.2,.3], label='Edge-Popup+Recycle', )
 sns.lineplot(x=[0.1,0.25,0.5,1], y=[58.5,72.49,77.55,79.9] ,ax=axs[0],linestyle='--',label='Baseline (Learned Weights)', legend=False)
 axs[0].set_title(label='Wide Conv-2', fontdict = {'fontsize' : 16})
 
-sns.lineplot(x=[0.1,0.25,0.5,1], y=[50.4,73.23,82.6,87.42] ,ax=axs[1],linestyle='-.', label='Biprop', legend=False)
-#sns.lineplot(x=[0.1,0.25,0.5,1], y=[59.64,78.51,86.5,88.91] , ci=[2,3,.08,.67], err_style='bars',ax=axs[1], label='Biprop+Recycle',linewidth =2, legend=False)
-axs[1].errorbar([0.1,0.25,0.5,1], [59.64,78.51,86.5,88.91] , [2,1,.08,.67], label='Biprop+Recycle',linewidth =2, )
 
+sns.lineplot(x=[0.1,0.25,0.5,1], y=[50.4,73.23,82.6,87.42] ,ax=axs[1],linestyle='-.', label='Biprop', legend=False)
+axs[1].errorbar([0.1,0.25,0.5,1], [59.64,78.51,86.5,88.91] , [2,1,.3,.67], label='Biprop+Recycle',linewidth =2, )
 sns.lineplot(x=[0.1,0.25,0.5,1], y=[50.73,73.34,82.08,86.67] , ax=axs[1],linestyle='-.', label='Edge-Popup', legend=False)
-sns.lineplot(x=[0.1,0.25,0.5,1], y=[59.82,78.92,85.63,88.5] ,ax=axs[1], label='Edge-Popup+Recycle',linewidth =2, legend=False)
+axs[1].errorbar([0.1,0.25,0.5,1], [59.82,78.92,85.63,88.5] ,[1.5,1,.25,.5], label='Edge-Popup+Recycle',linewidth =2, )
 sns.lineplot(x=[0.1,0.25,0.5,1], y=[67.26,81.13,84.96,86.66] ,linestyle='--',ax=axs[1], label='Baseline (Learned Weights)', legend=False)
 
 axs[1].set_title(label='Wide Conv-4', fontdict = {'fontsize' : 16})
@@ -25,14 +26,16 @@ axs[1].set_title(label='Wide Conv-4', fontdict = {'fontsize' : 16})
 
 
 sns.lineplot(x=[0.1,0.25,0.5,1], y=[56.9,77.84,86.23,89.52] ,ax=axs[2],linestyle='-.', label='Biprop', legend=False)
-sns.lineplot(x=[0.1,0.25,0.5,1], y=[65,77.9,88.43,90.9] ,ax=axs[2], label='Biprop+Recycle',linewidth =2, legend=False)
+axs[2].errorbar([0.1,0.25,0.5,1], [65,80.9,88.43,90.9] ,[.2,.6,.4,.52], label='Biprop+Recycle',linewidth =2, )
 sns.lineplot(x=[0.1,0.25,0.5,1], y=[57.3,78.48,86.24,89.53] ,ax=axs[2],linestyle='-.', label='Edge-Popup', legend=False)
-sns.lineplot(x=[0.1,0.25,0.5,1], y=[64.1,83,88.57,90.9] ,ax=axs[2], label='Edge-Popup+Recycle',linewidth =2, legend=False)
+axs[2].errorbar([0.1,0.25,0.5,1], [64.1,83,88.57,90.9] ,[2,.4,.22,.47], label='Edge-Popup+Recycle',linewidth =2, )
 sns.lineplot(x=[0.1,0.25,0.5,1], y=[72.48,83.65,87.54,89.14] ,linestyle='--',ax=axs[2], label='Baseline (Learned Weights)', legend=False)
 axs[2].set_title(label='Wide Conv-6', fontdict = {'fontsize' : 16})
 
 sns.lineplot(x=[0.1,0.25,0.5,1], y=[61.2,80.4,87.33,90.35] ,ax=axs[3],linestyle='-.', label='Biprop', legend=False)
-sns.lineplot(x=[0.1,0.25,0.5,1], y=[69,84.51,89.67,91] ,ax=axs[3], label='Biprop+Recycle',linewidth =2, legend=False)
+axs[3].errorbar([0.1,0.25,0.5,1], [69,84.51,89.67,91] ,[2.5,.58,.29,.12], label='Biprop+Recycle',linewidth =2, )
+sns.lineplot(x=[0.1,0.25,0.5,1], y=[63.35,81.5,87.92,90.28] ,ax=axs[3],linestyle='-.', label='Edge-Popup', legend=False)
+axs[3].errorbar([0.1,0.25,0.5,1], [70,85.5,89.73,91.36] ,[2.23,.67,.21,.14], label='Edge-Popup+Recycle', linewidth =2, )
 sns.lineplot(x=[0.1,0.25,0.5,1], y=[71.56,85.5,87.99,91.11] ,linestyle='--',ax=axs[3], label='Baseline (Learned Weights)', legend=False,)
 axs[3].set_title(label='Wide Conv-8', fontdict = {'fontsize' : 16})
 
@@ -42,11 +45,16 @@ for ax in axs:
 axs[0].set_ylabel("CIFAR-10 Test Accuracy",fontdict = {'fontsize' : 16} )
 
 plt.xticks([.1,.25, .5, 1])
-plt.xlim([.1, 1])
+plt.xlim([.098, 1.002])
 #handles, labels = axs[0].get_legend_handles_labels()
 #fig.legend(handles, labels, loc='lower left', ncol=3,bbox_to_anchor=(.12, .02))
 handles, labels = axs[1].get_legend_handles_labels()
-fig.legend(handles, labels, loc='lower left', ncol=5,bbox_to_anchor=(.18, .01), prop={'size': 18})
+
+handles=[handles[0], handles[3], handles[1], handles[4], handles[2]]
+labels=[labels[0], labels[3], labels[1], labels[4], labels[2]]
+print(handles)
+print(labels)
+fig.legend(handles, labels, loc='lower left', ncol=5,bbox_to_anchor=(.04, .001), prop={'size': 22})
 plt.tight_layout(rect=[0,.1,1,1])
 
 #plt.show()
--- figures/parameters_small.py
+++ figures/parameters_small.py
@@ -7,12 +7,29 @@ plt.clf()
 sns.set_style('whitegrid')
 fig, axs = plt.subplots(1, col, sharex=True, figsize=(5*col,5))
 
-
+'''
 sns.lineplot(x=[2150496,430099,860198,1720396,2580595,3440793], y=[81.36,76.9,80.23,81.36,81.2,80.28] ,ax=axs[0],linestyle='-.', label='Conv-2', legend=False)
 sns.lineplot(x=[1212512.00,242502.40,485004.80,970009.60,1455014.40,1940019.20], y=[88.91,84.3,87.75,88.64,88.8,88.05] ,ax=axs[0],linestyle='-.', label='Conv-4', legend=False)
 sns.lineplot(x=[1130592.00,226118.40,452236.80,904473.60,1356710.40,1808947.20], y=[90.9,85.5,90,90.6,90.8,90.2] ,ax=axs[0], label='Conv-6', legend=False)
 sns.lineplot(x=[2637920.00,527584.00,1055168.00,2110336.00,3165504.00,4220672.00], y=[91,86.4,90.55,90.85,90.93,90.35] ,ax=axs[0], label='Conv-8', legend=False)
 sns.lineplot(x=[2425024,2261184,5275840,11678912], y=[87, 89, 89.4,93] ,markers='.',linestyle='--',ax=axs[0], label='Baseline (Learned Weights)', legend=False)
+'''
+
+#sns.lineplot(x=[19881	134808	538160	2150496], y=[] ,ax=axs[0], label='Conv-2-Widths (EPU)', legend=False)
+#sns.lineplot(x=[11253	76184	303664	1212512], y=[] ,ax=axs[0], label='Conv-4-Widths', legend=False)
+#sns.lineplot(x=[10815	71064	283184	1130592], y=[] ,ax=axs[0], label='Conv-6-Widths', legend=False)
+#sns.lineplot(x=[25807	165016	659696	2637920], y=[] ,ax=axs[0], label='Conv-8-Widths', legend=False)
+
+#sns.lineplot(x=[19881,134808,538160,2150496], y=[52.55,68.8,76.66,81.36] ,ax=axs[0], label='Conv-2-Widths', legend=False)
+sns.lineplot(x=[76184,303664,1212512], y=[78.51,86.5,88.91] ,ax=axs[0], label='Conv-4-Widths', legend=False)
+sns.lineplot(x=[71064,283184,1130592], y=[82,88.43,90.9] ,ax=axs[0], label='Conv-6-Widths', legend=False)
+#sns.lineplot(x=[25807,165016,659696,2637920], y=[69,84.51,89.67,91] ,ax=axs[0], label='Conv-8-Widths', legend=False)
+
+#sns.lineplot(x=[39761,269616,1076320,4300992], y=[58.5,72.49,77.55,79.9] ,ax=axs[0], label='Conv-2-Widths (Baselines)', legend=False)
+sns.lineplot(x=[152368,607328,2425024], y=[81.13,84.96,86.66] ,ax=axs[0], label='Conv-4-Widths (Baselines)', legend=False)
+sns.lineplot(x=[142128,566368,2261184], y=[83.65,87.54,89.14] ,ax=axs[0], label='Conv-6-Widths (Baselines)', legend=False)
+#sns.lineplot(x=[51614,330032,1319392,5275840], y=[75.06,84.75,87.18,89.41] ,ax=axs[0], label='Conv-8-Widths (Baselines)', legend=False)
+
 axs[0].set_title(label='Wide Conv-4', fontdict = {'fontsize' : 18})
 
 
