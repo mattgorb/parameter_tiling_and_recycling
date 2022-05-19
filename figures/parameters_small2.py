4bfe082a0ecb6786b18a0564b4ee463480029860
matthewgorbett <Idontknow8112@@@>
2022-05-18 18:12:17 -0600

    r18

--- figures/parameters_small2.py
+++ figures/parameters_small2.py
@@ -30,17 +30,22 @@ sns.lineplot(x=[2425024,2261184,5275840,11678912], y=[87, 89, 89.4,93] ,markers=
 #sns.lineplot(x=[142128,566368,2261184], y=[83.65,87.54,89.14] ,ax=axs[0], label='Conv-6-Widths (Baselines)', legend=False)
 #sns.lineplot(x=[51614,330032,1319392,5275840], y=[75.06,84.75,87.18,89.41] ,ax=axs[0], label='Conv-8-Widths (Baselines)', legend=False)
 
-sns.lineplot(x=[48500.48,113059.20,121251.20,226118.40,242502.40,430099.20,452236.80,485004.80],
-             y=[41.6,68.8,64.72,81.22,79.78,68.87,86.94,85.34] ,ax=axs, label='IteRand', legend=False)
-sns.lineplot(x=[48500.48,113059.20,121251.20,226118.40,242502.40,430099.20,452236.80,485004.80],
-             y=[35.28,60.63,57.61,79.57,74.42,64.05,85.4,83.28] ,ax=axs, label='Edge-Popup', legend=False)
-sns.lineplot(x=[48500.48,113059.20,121251.20,226118.40,242502.40,430099.20,452236.80,485004.80],
-             y=[44,66.12,64.83,82.38,79,70.1,87.5,85.34] ,ax=axs, label='Biprop', legend=False)
-sns.lineplot(x=[48500.48,113059.20,121251.20,226118.40,242502.40,430099.20,452236.80,485004.80],
-             y=[49.38,72.11,73.5,85.5,84.3,76.9,90,87.75] ,ax=axs, label='Weight Recycle', legend=False)
+axs.errorbar([48500.48,113059.20,121251.20,226118.40,242502.40,430099.20,452236.80,485004.80],
+             [49.38,72.11,73.5,85.5,84.3,76.9,90,87.75],[1.23,1.74,1.55,1.37,1.34,1.18, 1.12, 1.] , label='Weight Recycle', )
 
+sns.lineplot(x=[48500.48,113059.20,121251.20,226118.40,242502.40,430099.20,452236.80,485004.80],
+             y=[41.6,68.8,64.72,81.22,79.78,68.87,86.94,85.34] ,ax=axs, label='IteRand',marker="o", markersize=4, legend=False)
+sns.lineplot(x=[48500.48,113059.20,121251.20,226118.40,242502.40,430099.20,452236.80,485004.80],
+             y=[35.28,60.63,57.61,79.57,74.42,64.05,85.4,83.28] ,ax=axs, label='Edge-Popup', marker="v", markersize=6,linestyle='-.',legend=False)
+sns.lineplot(x=[48500.48,113059.20,121251.20,226118.40,242502.40,430099.20,452236.80,485004.80],
+             y=[44,66.12,64.83,82.38,79,70.1,87.5,85.34] ,ax=axs,linestyle='-.',marker="^", markersize=6, label='Biprop', legend=False)
 
-
+axs.text(48500.48,33,"Conv-4, 98% Pruned (2.4M)",weight='bold',fontsize=8,)
+#axs.text(75500.48,75,"Conv-6, 95% \nPruned (2.2M)",weight='bold',fontsize=8,)
+axs.text(121251.48,55,"Conv-4, 95% (2.4M)",weight='bold',fontsize=8,)
+axs.text(226118.48,87.75,"Conv-2, 95% (4.3M)",weight='bold',fontsize=8,)
+axs.text(245502.48,84.9,"Conv-6, 90% (2.2M)",weight='bold',fontsize=8,)
+axs.text(415236.48,91.5,"Conv-6, 80% (2.2M)",weight='bold',fontsize=8,)
 
 
 #for ax in axs:
@@ -51,7 +56,7 @@ axs.set_ylabel("CIFAR-10 Test Accuracy",fontdict = {'fontsize' : 15} )
 axs.set(xticklabels=["",100,200,300,400,500])
 
 #plt.xticks()
-#plt.xlim([.1, 1])
+plt.ylim([25,95])
 #handles, labels = axs[0].get_legend_handles_labels()
 #fig.legend(handles, labels, loc='lower left', ncol=3,bbox_to_anchor=(.12, .02))
 handles, labels = axs.get_legend_handles_labels()
